3faecc5ad7332ffaf6b0752921b2e978cf5f6665
abbadonref <jokerokerok79@gmail.com>
2023-07-10 00:22:32 +0300

    function call export_sheets moved to main

--- export_sheets.py
+++ export_sheets.py
@@ -1,21 +1,11 @@
 import pygsheets
 import pandas as pd
-import argparse
-
 
 INT_MASS = [{
     "one": 1,
     "two": 2,
     "what?": 3
 }]
-def arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--csv_path', type=str, required=True, help='Specify path to output csv file')
-    parser.add_argument('--google_token', type=str, required=False, help='Specify path to google token file')
-    parser.add_argument('--table_id', type=str, required=False, help='Specify Google sheet document id (can find in url)')
-    parser.add_argument('--sheet_id', type=str, required=False, help='Specify title for a sheet in a document in which data will be printed')
-    args = parser.parse_args()
-    return args
 
 def write_data_to_table(csv_path, google_token, table_id, sheet_id):
     if google_token and sheet_id and table_id :
@@ -41,10 +31,3 @@ def write_data_to_table(csv_path, google_token, table_id, sheet_id):
     wk_content.set_dataframe(df, 'A1', copy_head=True)
 
 
-def main():
-    args = arg_parser()
-    write_data_to_table(args.csv_path, args.google_token, args.table_id, args.sheet_id)
-
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import argparse
 import git_logger
-
+import export_sheets
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", help="log pull requests", action="store_true")
     parser.add_argument("-i", help="log issues", action="store_true")
+    parser.add_argument("-e", help="log issues", action="store_true")
     parser.add_argument('-t', '--token', type=str, required=True, help='token github account')
     parser.add_argument('-l', '--list', type=str, required=True, help='repos names file')
     parser.add_argument('-o', '--out', type=str, required=True, help='output filename')
+    parser.add_argument('--google_token', type=str, required=False, help='Specify path to google token file')
+    parser.add_argument('--table_id', type=str, required=False,
+                        help='Specify Google sheet document id (can find in url)')
+    parser.add_argument('--sheet_id', type=str, required=False,
+                        help='Specify title for a sheet in a document in which data will be printed')
+    args = parser.parse_args()
+    if args.e:
+        for action in parser._actions:
+            if action.dest == 'google_token':
+                action.required = True
+            if action.dest == 'table_id':
+                action.required = True
+            if action.dest == 'sheet_id':
+                action.required = True
     return parser.parse_args()
 
 
@@ -25,10 +40,17 @@ def main():
     else:
         if not args.p and not args.i:
             git_logger.log_commits(client, repositories, csv_name)
+            if (args.e):
+                export_sheets.write_data_to_table(csv_name, args.google_token, args.table_id, args.sheet_id)
         if args.p:
             git_logger.log_pull_requests(client, repositories, csv_name)
+            if (args.e):
+                export_sheets.write_data_to_table(csv_name, args.google_token, args.table_id, args.sheet_id)
         if args.i:
             git_logger.log_issues(client, repositories, csv_name)
+            if (args.e):
+                export_sheets.write_data_to_table(csv_name, args.google_token, args.table_id, args.sheet_id)
+
 
 
 if __name__ == '__main__':
