b0e4c01eef03c370df41049d0e5b7c8953d520cd
SaveliyBorivets <borivets.ru3@gmail.com>
2024-02-04 05:02:37 +0300

    Пока без экспорта в таблицы

--- wikipars.py
+++ wikipars.py
@@ -6,34 +6,29 @@ def delete_everything_in_folder(folder_path):
     shutil.rmtree(folder_path)
     os.mkdir(folder_path)
 
-path = "/home/hom1e/KAFEDRAAAAA/local" #Путь к директории для репозитория
+path = "/home/hom1e/KAFEDRAAAAA/local"  #Путь к директории для репозитория
 
-#удаляем содержимое папки
-delete_everything_in_folder(path)
+#Создаем список репозиториев из файла
+with open("repolist", 'r') as file:
+    list_repos = [x for x in file.read().split('\n') if x]
 
-#Клонируем репозиторий в папку
-repo_url = "https://github.com/OSLL/github_repo_commitment_calc.wiki.git"
-repo = Repo.clone_from(repo_url, path)
+for name_rep in list_repos:
+    print("=================", name_rep, "=================")
+    #Удаляем содержимое папки
+    delete_everything_in_folder(path)
+    #Клонируем репозиторий в папку
+    repo_url = f"https://github.com/{name_rep}.wiki.git"
+    repo = Repo.clone_from(repo_url, path)
 
-prev_commits = list(repo.iter_commits(all=True, max_count=10))  # last 10 commits from all branches
+    prev_commits = list(repo.iter_commits(all=True))
 
-next_commit_ind = 1
-for commit in prev_commits[:-1]:
-    #print(commit.author, time.asctime(time.localtime(commit.committed_date)), "here page", commit, commit.diff(prev_commits[next_commit_ind]), sep=" --- ")
-    print("login:", commit.author)
-    print("datetime:", time.asctime(time.localtime(commit.committed_date)))
-    print("page:", "Here page")
-    print("revision id:", commit)
-    print("action:", *[diff.change_type for diff in prev_commits[next_commit_ind].diff(commit)])
-    print("diff:", commit.diff())
-    print("-------------------------------")
-    """for diff in commit.diff(prev_commits[next_commit_ind]):
-        print(diff)
-        print(diff.change_type)"""
-    next_commit_ind += 1
-
-
-
-#help(commit.diff())
-#help(Diff)
-
+    activity = {"A" : "Страница добавлена", "M" : "Страница изменена", "D" : "Страница удалена", "R":"Страница переименована"}
+    for ind in range(len(prev_commits[:-1])):
+        commit = prev_commits[ind]
+        print("login:", commit.author)
+        print("datetime:", time.asctime(time.localtime(commit.committed_date)))
+        print("page:", *[diff.b_path for diff in prev_commits[ind + 1].diff(commit)])
+        print("revision id:", commit)
+        print("action:", *[activity[diff.change_type] for diff in prev_commits[ind + 1].diff(commit)])
+        print("diff:", commit.diff())
+        print("-------------------------------")
