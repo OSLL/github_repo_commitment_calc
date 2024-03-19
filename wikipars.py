01dd6706acd399de682717043849db621dba7c10
SaveliyBorivets <borivets.ru3@gmail.com>
2024-03-19 23:04:47 +0300

    New changes

--- wikipars.py
+++ wikipars.py
@@ -12,17 +12,23 @@ def wikiparser(client, repositories, csv_name):
     #Создаем список репозиториев из файла
     with open(repositories, 'r') as file:
         list_repos = [x for x in file.read().split('\n') if x]
+    error_repos = []
 
     for name_rep in list_repos:
         print("=================", name_rep, "=================")
         #Удаляем содержимое папки
         delete_everything_in_folder(path)
         #Клонируем репозиторий в папку
-        repo_url = f"https://github.com/{name_rep}.wiki.git"
-        repo = Repo.clone_from(repo_url, path)
+        try:
+            repo_url = f"git@github.com:{name_rep}.wiki.git"
+            repo = Repo.clone_from(repo_url, path)
+        except:
+            print("Ошибка: Несуществующий репозиторий")
+            error_repos.append(name_rep)
+            continue
 
+        #Вывод изменений
         prev_commits = list(repo.iter_commits(all=True))
-
         activity = {"A" : "Страница добавлена", "M" : "Страница изменена", "D" : "Страница удалена", "R":"Страница переименована"}
         for ind in range(len(prev_commits[:-1])):
             commit = prev_commits[ind]
@@ -33,3 +39,8 @@ def wikiparser(client, repositories, csv_name):
             print("action:", *[activity[diff.change_type] for diff in prev_commits[ind + 1].diff(commit)])
             print("diff:", commit.diff())
             print("-------------------------------")
+
+    #Вывод репозиториев, с которыми возникли ошибки
+    print("!=====Проблемные репозитории=====!")
+    for rep in error_repos:
+        print(rep)
